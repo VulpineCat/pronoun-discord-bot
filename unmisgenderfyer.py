a430257a71ea683aa5ca97908890b9439866549f
VulpineCat | Darian <hello@vulpinecat.com>
2018-06-10 16:02:23 +0200

    add help and rewrite assign commands

--- unmisgenderfyer.py
+++ unmisgenderfyer.py
@@ -35,6 +35,23 @@ async def start_or_join_routine(server):
     await check_or_create_role(server, ROLENAME_SHE)
     await check_or_create_role(server, ROLENAME_THEY)
 
+async def add_or_remove_role(message, rolename):
+    if member_has_role(message.author, rolename):
+        await CLIENT.add_roles(message.author, grab_role(message.server, rolename))
+        await CLIENT.send_message(message.channel, "Sucessfully added role!")
+    else:
+        await CLIENT.remove_roles(message.author, grab_role(message.server, rolename))
+        await CLIENT.send_message(message.channel, "Sucessfully removed role!")
+
+
+
+def member_has_role(member, rolename):
+    for role in member.roles:
+        if role.name == rolename:
+            return True
+    return False
+
+
 @CLIENT.event
 async def on_ready():
     for server in CLIENT.servers:
@@ -46,21 +63,35 @@ async def on_server_join(server):
 
 @CLIENT.event
 async def on_message(message):
-    if message.content.startswith('!he'):
-        await CLIENT.add_roles(message.author, grab_role(message.server, ROLENAME_HE))
-        await CLIENT.send_message(message.channel, "Sucessfully added role!")
+    if not message.content.startswith('!'):
+        return
+
+    elif message.content.startswith('!help') or message.content.startswith('!list'):
+        await CLIENT.send_message(
+            message.channel,
+            ("There are three gender pronoun roles available:\n\n" +
+             "`She/Her`      -        assign/unassign me with `!she`\n" +
+             "`He/Him`       -        assign/unassign me with `!he`\n" +
+             "`They/Them`    -        assign/unassign me with `!they`\n\n\n" +
+             "Anything else you need? Hit up my **owner** at VulpineCat#0001 or `hello@vulpinecat.com`\n\n" +
+             "Love You\n- ***Unmisgenderfyer**"
+            ))
+
+
+    elif message.content.startswith('!he'):
+        await add_or_remove_role(message, ROLENAME_HE)
+    elif message.content.startswith('!she'):
+        await add_or_remove_role(message, ROLENAME_SHE)
+    elif message.content.startswith('!they'):
+        await add_or_remove_role(message, ROLENAME_THEY)
+
+
     elif message.content.startswith('!unhe'):
         await CLIENT.remove_roles(message.author, grab_role(message.server, ROLENAME_HE))
         await CLIENT.send_message(message.channel, "Sucessfully removed role!")
-    elif message.content.startswith('!she'):
-        await CLIENT.add_roles(message.author, grab_role(message.server, ROLENAME_SHE))
-        await CLIENT.send_message(message.channel, "Sucessfully added role!")
     elif message.content.startswith('!unshe'):
         await CLIENT.remove_roles(message.author, grab_role(message.server, ROLENAME_SHE))
         await CLIENT.send_message(message.channel, "Sucessfully removed role!")
-    elif message.content.startswith('!they'):
-        await CLIENT.add_roles(message.author, grab_role(message.server, ROLENAME_THEY))
-        await CLIENT.send_message(message.channel, "Sucessfully added role!")
     elif message.content.startswith('!unthey'):
         await CLIENT.remove_roles(message.author, grab_role(message.server, ROLENAME_THEY))
         await CLIENT.send_message(message.channel, "Sucessfully removed role!")
