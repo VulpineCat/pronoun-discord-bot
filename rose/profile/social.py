929a64531f1bcd3366a4f22772f53ef6d7388b18
Praxis | Darian <hello@vulpinecat.com>
2019-04-15 09:43:57 +0200

    adding telegram field

--- rose/profile/social.py
+++ rose/profile/social.py
@@ -29,3 +29,35 @@ class TwitterProfileField:
     def extract_username(self, value):
         import re
         return re.findall(r'(\w+$|\w+(?=/?$))', value)[0]
+
+class TelegramProfileField:
+    def __init__(self, value):
+        self._KEY = "Telegram"
+        self._username = self.extract_username(value)
+
+    @property
+    def username(self):
+        """Username on Telegram"""
+        return self._username
+
+    @username.getter
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self, value):
+        self._username = self.extract_username(self, value)
+
+    @property
+    def url(self):
+        """URL to profile on Telegram"""
+        return 'https://t.me/{}'.format(self._username)
+
+    @property
+    def flavour_text(self):
+        """Flavour text of service"""
+        return "Have fun chatting!"
+
+    def extract_username(self, value):
+        import re
+        return re.findall(r'(\w+$|\w+(?=/?$))', value)[0]
--- rose/tests/test_profile.py
+++ rose/tests/test_profile.py
@@ -1,32 +1,63 @@
 import pytest
 import rose.profile.social as social
 
-class TestProfileField(object):
 
-    def test_twitter_url(self):
-        twitter_profile = social.TwitterProfileField("https://twitter.com/real_praxis")
-        assert twitter_profile.username == "real_praxis"
+class TestProfileFields(object):
+    class TestTwitterField:
+        def test_twitter_url(self):
+            twitter_profile = social.TwitterProfileField("https://twitter.com/real_praxis")
+            assert twitter_profile.username == "real_praxis"
 
-    def test_twitter_odd_url(self):
-        twitter_profile = social.TwitterProfileField("https://twitter.com/real_praxis/")
-        assert twitter_profile.username == "real_praxis"
+        def test_twitter_odd_url(self):
+            twitter_profile = social.TwitterProfileField("https://twitter.com/real_praxis/")
+            assert twitter_profile.username == "real_praxis"
 
-    def test_twitter_at(self):
-        twitter_profile = social.TwitterProfileField("@real_praxis")
-        assert twitter_profile.username == "real_praxis"
+        def test_twitter_at(self):
+            twitter_profile = social.TwitterProfileField("@real_praxis")
+            assert twitter_profile.username == "real_praxis"
 
-    def test_twitter_username(self):
-        twitter_profile = social.TwitterProfileField("real_praxis")
-        assert twitter_profile.username == "real_praxis"
+        def test_twitter_username(self):
+            twitter_profile = social.TwitterProfileField("real_praxis")
+            assert twitter_profile.username == "real_praxis"
 
-    def test_twitter_generate_url(self):
-        twitter_profile = social.TwitterProfileField("real_praxis")
-        assert twitter_profile.url == "https://twitter.com/real_praxis"
+        def test_twitter_generate_url(self):
+            twitter_profile = social.TwitterProfileField("real_praxis")
+            assert twitter_profile.url == "https://twitter.com/real_praxis"
 
-    def test_twitter_flavour_text(self):
-        twitter_profile = social.TwitterProfileField("real_praxis")
-        assert twitter_profile.flavour_text == ":bird: Tweet Tweet :bird:"
+        def test_twitter_flavour_text(self):
+            twitter_profile = social.TwitterProfileField("real_praxis")
+            assert twitter_profile.flavour_text == ":bird: Tweet Tweet :bird:"
 
-    def test_twitter_empty_input(self):
-        with pytest.raises(TypeError):
-            twitter_profile = social.TwitterProfileField()
+        def test_twitter_empty_input(self):
+            with pytest.raises(TypeError):
+                twitter_profile = social.TwitterProfileField()
+
+    class TestTelegramField:
+        def test_telegram_url(self):
+            telegram_profile = social.TelegramProfileField("https://t.me/good_praxis")
+            assert telegram_profile.username == "good_praxis"
+
+        def test_telegram_odd_url(self):
+            telegram_profile = social.TelegramProfileField("https://t.me/good_praxis/")
+            assert telegram_profile.username == "good_praxis"
+
+        def test_telegram_at(self):
+            telegram_profile = social.TelegramProfileField("@good_praxis")
+            assert telegram_profile.username == "good_praxis"
+
+        def test_telegram_username(self):
+            telegram_profile = social.TelegramProfileField("good_praxis")
+            assert telegram_profile.username == "good_praxis"
+
+        def test_telegram_generate_url(self):
+            telegram_profile = social.TelegramProfileField("good_praxis")
+            assert telegram_profile.url == "https://t.me/good_praxis"
+
+        def test_telegram_flavour_text(self):
+            telegram_profile = social.TelegramProfileField("good_praxis")
+            assert telegram_profile.flavour_text == "Have fun chatting!"
+
+        def test_telegram_empty_input(self):
+            with pytest.raises(TypeError):
+                telegram_profile = social.TelegramProfileField()
+
